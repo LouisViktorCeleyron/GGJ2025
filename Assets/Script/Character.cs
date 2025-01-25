53e2c2119fb4888d3f307def36069114bb53159a
Louis Celeyron <50552167+LouisViktorCeleyron@users.noreply.github.com>
2025-01-25 09:38:06 +0100

    Stuffs

--- Assets/Script/Character.cs
+++ Assets/Script/Character.cs
@@ -8,24 +8,33 @@ public class Character : MonoBehaviour
     private Rigidbody _rgbd;
 
     [SerializeField] 
-    private float _speed;
+    private float _speed, _xBoost=1.5f, _zBoost=1.1f, _yBoost = 2f;
     private Vector3 _velocity;
+
+    private float _hAxis, _vAxis;
 
     void Update()
     {
-        _velocity.x = Input.GetAxis("Horizontal");
-        _velocity.z = Input.GetAxis("Vertical");
-
     }
 
     private void FixedUpdate()
     {
-        _velocity = _velocity.normalized*_speed*Time.deltaTime;
-        _rgbd.MovePosition(transform.position + _velocity);   
+        _velocity.x = _hAxis = Input.GetAxisRaw("Horizontal");
+        _velocity.z = _vAxis = Input.GetAxisRaw("Vertical");
+
+        _velocity = _velocity.normalized*_speed*Time.fixedDeltaTime;
+
+        _rgbd.MovePosition(_rgbd.position + _velocity);   
     }
 
-    private void Move()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        var bubbleCollision = collision.gameObject.GetComponent<BubbleMovement>();
+        if(bubbleCollision != null)
+        {
+            var frwrdV = Vector3.forward * Mathf.Max(.5f, Mathf.Abs(_hAxis)* _zBoost);
+            var rightV = Vector3.right * Mathf.Max(1, _vAxis * _xBoost);
+            bubbleCollision.Bounce(rightV+frwrdV+Vector3.up*_yBoost);
+        }
     }
 }
